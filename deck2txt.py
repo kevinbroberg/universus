9ad40b2290facb4dee20f65b9d2faff66c4925ed
kevinbroberg <kevin.broberg@gmail.com>
2021-01-24 10:55:10 -0500

    Initial commit of python scripts

---
+++ deck2txt.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+import json
+import argparse
+from collections import Counter
+
+parser = argparse.ArgumentParser(description='Transforms a TTS object into a text deck.')
+parser.add_argument('deck', metavar='deck', type=str,
+                    help='deck json file to read. should end in .json')
+args = parser.parse_args()
+sauce = args.deck
+deck_name = sauce.partition('.json')[0]
+
+with open(sauce, "r") as read:
+    data = json.load(read)
+
+contained = data["ObjectStates"][0]
+cards = [card["Nickname"].lower() for card in contained["ContainedObjects"]]
+print Counter(cards)
+deck = ["{} {}".format(qty, name) for name, qty in Counter(cards).items()]
+
+with open(deck_name + ".txt", "w") as outfile:
+    outfile.write("\n".join(deck))
---
+++ txt2deck.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+import re
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description='Transforms a txt decklist into TTS object.')
+parser.add_argument('infile', metavar='file', type=str,
+                    help='name of file to read')
+args = parser.parse_args()
+deck_spec = args.infile
+deck_name = deck_spec.partition('.txt')[0]
+
+with open(deck_spec, "r") as read:
+    deck_in = read.readlines()
+
+
+def tuples_to_deck(tuples):
+    return [cards[name[1]] for name in tuples if name[1] in cards for _ in range(int(name[0]))]
+
+
+ stack = "2781_stack.json"
+
+target_dir = ""
+
+
+# TODO implement sideboard loading
+sideboard = """
+1 ninja outcast
+1 superior technique
+2x Power Struggles
+
+1 undying rage
+1 space land
+"""
+# deck_in = deck_in.replace('\xe2\x80\x99', "'")
+
+# TODO modularize, use the thing i built for Tim
+# TODO design script input / output
+
+regex = re.compile(r'\s*(\d+)x?\)? (.*?)\s*$')  # TODO capture characters but not their "dots" (which are weird unicode shit)
+deck = [(m.group(1), m.group(2).lower()) for l in deck_in for m in [regex.search(l)] if m]
+# deckToMatch = [name[1] for name in deck for times in range(int(name[0]))]
+
+with open(stack, "r") as read:
+    data = json.load(read)
+
+contained = data["ObjectStates"][0]
+cards = {card["Nickname"].lower(): card for card in contained["ContainedObjects"]}
+
+exact_matches = [what for what in deck if what[1] in cards]
+deckToBuild = tuples_to_deck(deck)  # TODO append Levenshtein selections after this. if it worked
+
+print(len(deckToBuild))
+unmatched = [name[1] for name in deck if name[1] not in cards]
+
+if unmatched:
+    print(unmatched)
+else:
+    print("No missing cards")
+ids = [card["CardID"] for card in deckToBuild]
+
+contained["ContainedObjects"] = deckToBuild
+contained["Nickname"] = deck_name
+contained["DeckIDs"] = ids
+dest = deck_name + ".json"
+
+if target_dir:
+    dest = target_dir + "/" + dest
+
+with open(dest, "w") as write:
+    json.dump(data, write, indent=2)
+
+
+def search(name):
+    return [card for card in cards if name in card]
